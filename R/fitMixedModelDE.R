abe2b99f47c135f692165fc1305db1e8f3fb1060
Alex Pickering <alexvpickering@gmail.com>
2020-03-10 09:57:06 -0400

    lmerTest::lmer explicit
    without this was getting lmerMod (lme4) instead of lmerModLmerTest (lmerTest)
    which resulted in call to as_lmerModLmerTest for each contrast --> very slow.
    This speed up things dramatically for large models.

--- R/fitMixedModelDE.R
+++ R/fitMixedModelDE.R
@@ -400,7 +400,6 @@ getContrast = function( exprObj, formula, data, coefficient, L){
 #' @rdname dream-method
 #' @importFrom pbkrtest get_SigmaG
 #' @importFrom BiocParallel bpiterate bpparam
-#' @importFrom lmerTest lmer
 dream <- function( exprObj, formula, data, L, fitInit, Linit = NULL, return.resList = FALSE, return.fitInit = FALSE, ddf = c("Satterthwaite", "Kenward-Roger"), REML=TRUE, useWeights=TRUE, weightsMatrix=NULL, control = lme4::lmerControl(calc.derivs=FALSE, check.rankX="stop.deficient" ),suppressWarnings=FALSE, quiet=FALSE, BPPARAM=bpparam(), computeResiduals=FALSE, ...){ 
 
 	exprObjInit = exprObj
@@ -541,8 +540,7 @@ dream <- function( exprObj, formula, data, L, fitInit, Linit = NULL, return.resL
 
 		timeStart = proc.time()
 		if (missing(fitInit)) {
-		  fitInit <- lmer( eval(parse(text=form)), data=data,..., REML=REML, control=control )
-
+		  fitInit <- lmerTest::lmer( eval(parse(text=form)), data=data,..., REML=REML, control=control )
 		  if (return.fitInit) return(fitInit)
 		  
 		}
@@ -596,7 +594,7 @@ dream <- function( exprObj, formula, data, L, fitInit, Linit = NULL, return.resL
  
 			# fit linear mixed model
 			suppressWarnings({
-				fit <- lmer( eval(parse(text=form)), data=data2, REML=REML,..., weights=gene14643$weights, control=control,na.action=na.action)
+				fit <- lmerTest::lmer( eval(parse(text=form)), data=data2, REML=REML,..., weights=gene14643$weights, control=control,na.action=na.action)
 				})
 			# , start=theta
 
